94a4b41016ebc25382980daaece6fa767d672c3d
Ben DiFrancesco <ben@scopelift.co>
2021-04-01 18:07:48 -0400

    Configure hardhat for Rinkeby deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,42 @@
+require('dotenv').config()
+
+// plugins
 require("@nomiclabs/hardhat-truffle5");
 require("@nomiclabs/hardhat-ethers");
+
+const chainIds = {
+  mainnet: 1,
+  rinkeby: 4,
+};
+
+// Ensure that we have all the environment variables we need.
+let mnemonic = '';
+if (!process.env.MNEMONIC) {
+  console.warn('Please set your MNEMONIC in a .env file');
+} else {
+  mnemonic = process.env.MNEMONIC;
+}
+
+let infuraApiKey = '';
+if (!process.env.INFURA_API_KEY) {
+  console.warn('Please set your INFURA_API_KEY in a .env file');
+} else {
+  infuraApiKey = process.env.INFURA_API_KEY;
+}
+
+function createNetworkConfig(network) {
+  const url = 'https://' + network + '.infura.io/v3/' + infuraApiKey;
+  return {
+    accounts: {
+      count: 10,
+      initialIndex: 0,
+      mnemonic,
+      path: "m/44'/60'/0'/0",
+    },
+    chainId: chainIds[network],
+    url,
+  };
+}
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -11,5 +48,8 @@ module.exports = {
       enabled: true,
       runs: 999999,
     },
+  },
+  networks: {
+    rinkeby: createNetworkConfig('rinkeby'),
   },
 };
